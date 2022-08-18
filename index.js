ea0ee105f15dc8fdd681c29305af2408824fae7b
Abdur Rahman <abdur.rahman@tuhh.de>
2022-08-18 23:22:46 +0200

    set up server

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // backend index file
+
+/*
+* Package Imports
+* ---------------------------------------------------------------
+*/
+// express.js
+const express = require('express');
+//body-parser
+const bodyParser = require('body-parser');
+
+/*
+* Local Imports
+* ---------------------------------------------------------------
+*/
+
+/*
+* App Initialization
+* ---------------------------------------------------------------
+*/
+const app = express();
+
+/*
+* App Configurations
+* ----------------------------------------------------------------
+*/
+
+/*
+* Middlewares
+* ----------------------------------------------------------------
+*/
+
+
+/*
+* Routes
+* ----------------------------------------------------------------
+*/
+
+app.use((req,res,next)=>{
+    console.log('server is up & running')
+    res.send('<h1>SERVER is UP & RUNNING!! </h1>')
+})
+
+
+/*
+* Start server
+* ----------------------------------------------------------------
+*/
+
+app.listen(3000)
+
+
+
+
+
+
+
+
